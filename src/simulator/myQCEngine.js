e4b45321b3cd2653b80a81a5b7efc3b0a16e9656
git_user <22122184@zju.edu.cn>
2022-03-08 14:10:20 +0800

    minor improvements

--- src/simulator/myQCEngine.js
+++ src/simulator/myQCEngine.js
@@ -1181,8 +1181,8 @@ export default class QCEngine {
 
     get_evo_matrix(label_id)
     {
-        console.log(label_id);
-        console.log(this.labels);
+        //console.log(label_id);
+        //console.log(this.labels);
         //console.log(this.operations);
         
         let gate_mats = [];
@@ -1257,7 +1257,7 @@ export default class QCEngine {
             all_gate =dot(all_gate, column_res);
             
         }
-        console.log("all_gate",all_gate);
+        //console.log("all_gate",all_gate);
 
         
         let stru = this.get_input_state(label_id);
@@ -1292,7 +1292,7 @@ export default class QCEngine {
             }
         }
 
-        console.log("gate_mats", gate_mats);
+        //console.log("gate_mats", gate_mats);
         
 
         //fill fake data
