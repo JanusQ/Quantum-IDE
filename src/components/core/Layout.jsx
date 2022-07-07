4ce7e13eb4be647f48d6939a1ca2d8b01e6d588e
huguangze <2581504687@qq.com>
2022-07-07 16:06:10 +0800

    加logo

--- src/components/core/Layout.jsx
+++ src/components/core/Layout.jsx
@@ -4,6 +4,8 @@ import { PageHeader } from 'antd'
 import '../styles/Layout.css'
 import { useHistory } from 'react-router-dom'
 import Footer from './Footer'
+import { fromPairs } from 'lodash'
+import Logo from '../../images/logo.png'
 const Layout = ({ children, isHome, isLogin, isIde }) => {
   const history = useHistory();
   const backHome = () => {
@@ -12,16 +14,23 @@ const Layout = ({ children, isHome, isLogin, isIde }) => {
   return (
     <div
       style={{
-        background:  "#eaeff5" ,
+        background: "#eaeff5",
         height: isLogin ? "calc(100% - 50px)" : isIde ? "100%" : "auto",
       }}
     >
-      <div className="front_header" style={{height:isIde ? "50px":'80px',lineHeight:isIde ? "50px":"80px"}}>
+      <div
+        className="front_header"
+        style={{
+          height: isIde ? "50px" : "80px",
+          lineHeight: isIde ? "50px" : "80px",
+        }}
+      >
         <div className="front_menu_div">
           <span className="front_logo_title" onClick={backHome}>
+            <img src={Logo} alt="" style={{ width: 45, height: 45,marginRight: 10 ,	verticalAlign: 'middle',marginBottom: 5 }} />
             太元量子计算
           </span>
-          <Navigation isIde={isIde}/>
+          <Navigation isIde={isIde} />
         </div>
       </div>
       <div
