997138a0baaa5a08714cc828bbcf40bdb3a0f261
胡光泽 <2581504687@qq.com>
2023-02-13 16:57:53 +0800

    about en

--- src/components/core/AbuoutUs.jsx
+++ src/components/core/AbuoutUs.jsx
@@ -31,23 +31,51 @@ export default function AbuoutUs() {
             <p> {t("auboutUs.References")}：</p>
             <p>
               [1]
-              <a href="https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.119.180511">
+              <a
+                target="_blank"
+                href="https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.119.180511"
+              >
                 2017-Phys. Rev. Lett.《10-Qubit Entanglement and Parallel Logic
                 Operations with a Superconducting Circuit》
               </a>
             </p>
             <p>
               [2]
-              <a href=" https://www.science.org/doi/10.1126/science.aay0600">
+              <a
+                target="_blank"
+                href="https://www.science.org/doi/10.1126/science.aay0600"
+              >
                 2019-Science Advances《Generation of multicomponent atomic
                 Schrödinger cat states of up to 20 qubits》
               </a>
             </p>
             <p>
               [3]
-              <a href=" https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.127.240502">
+              <a
+                target="_blank"
+                href="https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.127.240502"
+              >
                 2021-Phys. Rev. Lett. 《Stark Many-Body Localization on a
                 Superconducting Quantum Processor》
+              </a>
+            </p>
+            <p>
+              [4]
+              <a
+                target="_blank"
+                href="https://www.nature.com/articles/s41586-022-04854-3"
+              >
+                2022-Nature 《Digital quantum simulation of Floquet
+                symmetry-protected topological phases》
+              </a>
+            </p>
+            <p>
+              [5]
+              <a
+                target="_blank"
+                href="https://www.science.org/doi/10.1126/science.ade6219"
+              >
+                2022-Science 《Observing the quantum topology of light》
               </a>
             </p>
           </div>
