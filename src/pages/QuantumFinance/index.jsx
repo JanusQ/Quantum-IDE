b37b5f507c1a8cd5e20fe0c00dca9fd9cb1a89e5
huguangze <2581504687@qq.com>
2025-02-11 15:53:47 +0800

    1

--- src/pages/QuantumFinance/index.jsx
+++ src/pages/QuantumFinance/index.jsx
@@ -4,7 +4,7 @@ export default function QuantumFinance() {
   return (
     <iframe
       style={{ width: '100%', height: '100vh' }}
-      src="http://janusq.zju.edu.cn:10317/"
+      src="http://192.168.9.11:8080/app"
       frameborder="0"
     ></iframe>
   )
